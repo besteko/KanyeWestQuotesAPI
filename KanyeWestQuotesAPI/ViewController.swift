ffd018a75337eb71929bd8d763e1d6a562a10e45
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-03-23 16:43:26 +0300

    Initial Commit

---
+++ KanyeWestQuotesAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanyeWestQuotesAPI
+//
+//  Created by Beste Kocaoglu on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
